6f232d242a1e17cc38e3a9668ff2c72357670077
Adel ESSAFI <adelessafi@gmail.com>
2020-12-04 15:26:09 +0100

    hosts

--- lab02-terraformec2/ec2.tf
+++ lab02-terraformec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "admin" {
   key_name   = "admin"
-  public_key = "......"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDENfxdQmP+DkUZTsCP/0i6dPpG3N9nX/8n3uXBSRAhuahj8DLZoh14nO+ZIMrLA2IlFbc5Lt+lx1ljLlAFZzdg0nzr5pwo66mfVhgEdyPLlAIp5XrF2SWHai0JZHyFl4obHPrLAvUeNesWksOGu2RKKIahiMjfXtyAuZEuBvLh8FDzUC375dEfPokDb789wy8hk2aNK7XL+QlUjFCCSq6O1KphXzXYBaTHQQ3D74sWAyDqSaXWE8kJi4g2jRVX+TmnNRWhYskftOqnonnBQAeGIeETJlxMOoP9UiZIZruLvXID397n+dlvbekI23sHUrWTGA7MwDeKsOw7ZAfcZf9RevwGBAuFBHKfrNT/n2YNT/SjQcE5V+ejZdU6odYJDAETdgGYrNQ7kLJAmAJekIySb+DFm7kUiA8kp7o8+blkWhBToIu3MsE9uJYrAkGBj1dw2BIwPdBHjm7tgkdLdso7hoELfDn7Wa3TuLMMe6HlVdc9w24jZQhP0zWWxHcjrC0= adelessafi@cloudaccess.local"
 }
 
 resource "aws_instance" "server1" {
--- lab02-terraformec2/security_group.tf
+++ lab02-terraformec2/security_group.tf
@@ -3,12 +3,12 @@ resource "aws_default_security_group" "default" {
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks     = ["83.68.192.4/32"]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
